4be8abf3c09dff74b3ffa873fde8afc869394c6c
Shawon Reza <shawon.reza.sr81@gmail.com>
2024-09-23 02:21:18 +0600

    check

--- js/commonFunction.js
+++ js/commonFunction.js
@@ -7,8 +7,6 @@ function inputValidation(id) {
     if (!isNaN(money) && money <= total && money > 0) {
         document.getElementById('my_modal_1').showModal
             ();
-        total -= money;
-        document.getElementById('totalBalanceId').innerText = total;
         return money;
     } else {
         alert("invalid")
@@ -17,21 +15,34 @@ function inputValidation(id) {
 
 // Update individual donated money
 function ammountDonatedValidation(id, money) {
-    let ammount = document.getElementById(id).innerText;
-    ammount = parseFloat(ammount);
-    ammount += money;
-    document.getElementById(id).innerText = ammount;
+    let total = document.getElementById('totalBalanceId').innerText
+    total = parseFloat(total)
+    if (money <= total) {
+        total -= money;
+        document.getElementById('totalBalanceId').innerText = total;
+        let ammount = document.getElementById(id).innerText;
+        ammount = parseFloat(ammount);
+        ammount += money;
+        document.getElementById(id).innerText = ammount;
+    }
+    else { return; }
+
 }
 
 // Update donation History
 function updateHistory(id, money) {
-    let date = new Date();
-    const p = document.getElementById(id).innerText;
-    const div = document.createElement('div');
-    div.innerHTML = `<div class="border-2 rounded-lg px-8 py-4 container mx-auto mb-4">
-   <h1 class="text-lg font-semibold">${money} Taka is ${p}</h1>
-   <p class="opacity-50">Date : ${date}</p>
-    </div>`
-    document.getElementById('historyContainerId').appendChild(div);
+    let total = document.getElementById('totalBalanceId').innerText
+    total = parseFloat(total)
+    if (money > 0) {
+        let date = new Date();
+        const p = document.getElementById(id).innerText;
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="border-2 rounded-lg px-8 py-4 container mx-auto mb-4">
+       <h1 class="text-lg font-semibold">${money} Taka is ${p}</h1>
+       <p class="opacity-50">Date : ${date}</p>
+        </div>`
+        document.getElementById('historyContainerId').appendChild(div);
+
+    } else { return; }
 
 }
